4e7c80bb7f459b33b5edeef410f3f7a4a1497391
sures <sures@LAPTOP-UNNEGM3M>
2021-12-16 19:06:42 +0530

    Servlet-first-app

---
+++ ServletFirstApp/src/com/createiq/hi/Hello.java
@@ -0,0 +1,36 @@
+package com.createiq.hi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		printWriter.write("<h1>Hello SERVLAET </h1>");;
+	}
+
+	
+
+}
